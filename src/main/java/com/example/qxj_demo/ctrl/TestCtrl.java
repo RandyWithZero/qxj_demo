6bf72775130bbf65a2c1722fa53166ad643d8324
RandyWithZero <38777687+RandyWithZero@users.noreply.github.com>
2019-09-26 15:12:09 +0800

    afageawgwaga

--- src/main/java/com/example/qxj_demo/ctrl/TestCtrl.java
+++ src/main/java/com/example/qxj_demo/ctrl/TestCtrl.java
@@ -18,6 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestCtrl {
     @RequestMapping("test")
     public String test(){
-        return "test OK -version:101.0";
+        return "test OK -version:10xx1.0";
     }
 }
